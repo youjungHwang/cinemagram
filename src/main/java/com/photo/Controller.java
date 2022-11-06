5314ec274172f3723a48dcfd1691c4bed0869a5f
youjungHwang <dbwjd5032@gmail.com>
2022-11-07 01:10:42 +0900

    Initial commit

---
+++ src/main/java/com/photo/Controller.java
@@ -0,0 +1,12 @@
+package com.photo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String basic() {
+        return "Hello world";
+    }
+}
